0dd51afd1330091b20739e7ab5a5024ea7100509
Marcin Michniak <michniak@yandex.com>
2018-04-22 13:26:35 +0200

    Create and render two simple functional components with jsx. Add some styling and logo for a header.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-alert("hello react!")
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Header from './components/common/HeaderComponent/header'
+import './index.css'
+
+const App = () => {
+    const title = 'Crypto Currency Watcher'
+    return (
+        <div>
+            <Header />
+            <h1>{title}</h1>
+            <p>Your source of crypto currency world informations.</p>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
