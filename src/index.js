b03cdfc3de08de7624d256a86c7c76379fed5ad2
Marcin Michniak <michniak@yandex.com>
2018-04-29 12:26:55 +0200

    Import routing packages. Add route for root and default route if path did not match any route.

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Header from './components/common/HeaderComponent/header'
 import List from './components/common/ListComponent/List'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import NotFound from './components/common/NotFoundComponent/NotFound'
+import Detail from './components/common/DetailComponent/Detail'
 import './index.css'
 
 const App = () => {
     const title = 'Crypto Currency Watcher'
     return (
-        <div>
-            <Header />
-            <h1>{title}</h1>
-            <List />
-        </div>
+        <BrowserRouter>
+            <div>
+                <Header />
+                <h1>{title}</h1>
+                <Switch>
+                    <Route path="/" component={List} exact />
+                    <Route path="/currency/:id" component={Detail} exact/>
+                    <Route component={NotFound}/>
+                </Switch>
+            </div>
+        </BrowserRouter>
     )
 }
 
